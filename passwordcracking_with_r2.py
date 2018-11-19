973373fc8a03ef6006006bc57e4ace56792cdaa6
root <root@localhost.localdomain>
2018-11-20 01:34:01 +0300

    cracking with radare2

---
+++ passwordcracking_with_r2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import r2pipe
+
+# open the binary with attached debugger
+r2 = r2pipe.open('./licence', ['-d'])
+
+while 'invalid' not in r2.cmd('s'):
+    # do one step + seek to rip register
+    r2.cmd('ds;sr rip')
+
+    # dissamble one instruction
+    json = r2.cmdj('pdj 1')
+
+    if not json:
+        continue
+
+    json = json[0]
+    opcode = json['opcode']
+
+    # identify an instruction with a password
+
+    if 'cmp byte [rcx]' in opcode:
+        # extract next character of a password
+        char = str(hex(json['ptr']))
+        # write the character to stack
+        r2.cmd('wx {} @ rcx'.format(char))
+        # print the character
+        print(chr(int(char, 16)), end='', flush=True)
+print()

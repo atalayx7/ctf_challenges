ee9664285bb8f11e3ffb13205ff4947a14180ef4
Atalay <atalay.ergen@std.antalya.edu.tr>
2019-04-04 16:19:47 +0300

    Delete xor.py

--- xor.py
+++
@@ -1,9 +0,0 @@
-
-def xor(data, key):
-
-    l = len(key)
-    decoded = ""
-    for i in range(0, len(data)):
-        decoded += chr(ord(data[i]) ^ ord(key[i % l]))
-    return decoded
-print xor('mydata', '2')

